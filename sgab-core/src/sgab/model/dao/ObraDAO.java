8ac39c5063dbeb35696c921c13657166e8cb7cb9
Mateus Sousa <mateussdamiano@gmail.com>
2023-06-27 16:13:54 -0300

    move method - 1, replace magic number with symbolic constant - 1

--- sgab-core/src/sgab/model/dao/ObraDAO.java
+++ sgab-core/src/sgab/model/dao/ObraDAO.java
@@ -8,6 +8,7 @@ import sgab.model.dto.Obra;
 import sgab.model.dto.util.ObraStatus;
 import sgab.model.exception.PersistenciaException;
 import sgab.model.dto.Autor;
+import sgab.util.Helpers;
 
 public class ObraDAO implements GenericDAO<Obra, Long>{
     private Map<Long, Obra> obras = new HashMap<>();
@@ -94,45 +95,10 @@ public class ObraDAO implements GenericDAO<Obra, Long>{
     public List<Obra> pesquisaTituloAproximado(String titulo){
         List<Obra> resultado = new LinkedList<>();
         for (Obra obra : listarObras()){
-            if(checaSemelhanca(obra.getTitulo(), titulo)){
+            if(Helpers.checaSemelhanca(obra.getTitulo(), titulo)){
                 resultado.add(obra);
             }
         }
         return resultado;
     }
-     
-    private boolean checaSemelhanca(String nomeObra, String nomeInput){
-        String nomeA = nomeObra.toLowerCase();
-        String nomeB = nomeInput.toLowerCase();
-        float count = 0;
-        
-        //iguala o tamanho das Strings
-        if(nomeA.length() > nomeB.length()){
-            for(int i =0; i < (nomeObra.length() - nomeInput.length()); i++){
-                nomeB = nomeB.concat(" ");
-            }
-        }
-        if(nomeA.length() < nomeB.length()){
-            for(int i =0; i < (nomeObra.length() - nomeInput.length()); i++){
-                nomeA = nomeA.concat(" ");
-            }
-        }
-        
-        for(int i=0, j=0; i<nomeA.length(); i++, j++) { 
-            if(nomeA.charAt(i) != nomeB.charAt(j)){
-                count++;
-                if(i+1 == nomeA.length())
-                    continue;
-                if(nomeA.charAt(i+1)==nomeB.charAt(j))
-                    j--;
-            }
-        }
-        float porcentagem = count / nomeA.length();
-        
-        if(porcentagem < 0.21)
-            return true;
-        
-        return false;
-    }
-    
 }
